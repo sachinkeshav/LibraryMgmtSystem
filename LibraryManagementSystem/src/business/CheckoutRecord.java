63795bac989e631c8648bd58ea14d1734558ee93
dheeraj <rythmnite66@gmail.com>
2015-07-29 10:33:27 -0500

    checkoutRecord feature updates

--- LibraryManagementSystem/src/business/CheckoutRecord.java
+++ LibraryManagementSystem/src/business/CheckoutRecord.java
@@ -1,6 +1,7 @@
 package business;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CheckoutRecord implements Serializable {
@@ -8,6 +9,11 @@ public class CheckoutRecord implements Serializable {
 	/**
 	 * 
 	 */
+
+	public CheckoutRecord() {
+		checkoutRecordEntries = new ArrayList<>();
+	}
+
 	private static final long serialVersionUID = 7086395344898382470L;
 	private List<CheckoutRecordEntry> checkoutRecordEntries;
 
--- LibraryManagementSystem/src/ui/controller/LibrarianController.java
+++ LibraryManagementSystem/src/ui/controller/LibrarianController.java
@@ -1,13 +1,23 @@
 package ui.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import business.CheckoutRecordEntry;
+import business.LibraryMember;
 import business.LibrarySystemException;
 import business.SystemController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class LibrarianController {
 
@@ -16,6 +26,17 @@ public class LibrarianController {
 
 	@FXML
 	private TextField isbn;
+
+	@FXML
+	private TableView table;
+	@FXML
+	private TableColumn memberIdCol;
+	@FXML
+	private TableColumn bookCol;
+	@FXML
+	private TableColumn issueDateCol;
+	@FXML
+	private TableColumn dueDateCol;
 
 	@FXML
 	public void searchButtonAction(ActionEvent evt) {
@@ -29,9 +50,10 @@ public class LibrarianController {
 
 				if (alert.getResult() == ButtonType.YES) {
 					checkout = mainController.checkoutBook(memberId.getText(), isbn.getText());
+					handleCheckoutButton();
 				}
 				if (checkout) {
-					Alert success = new Alert(AlertType.CONFIRMATION);
+					Alert success = new Alert(AlertType.INFORMATION);
 					success.setTitle("Success");
 					success.setHeaderText("Successfully checked out!");
 					success.show();
@@ -44,6 +66,28 @@ public class LibrarianController {
 			alert.setContentText(ex.getMessage());
 			alert.show();
 		}
+	}
+
+	public void handleCheckoutButton() {
+		SystemController mainController = SystemController.getInstance();
+		LibraryMember member = mainController.search(memberId.getText());
+		List<CheckoutRecordEntry> checkoutRecordEntries = member.getCheckoutRecord().getCheckoutRecordEntries();
+		CheckoutRecordEntry currentEntry = checkoutRecordEntries.get(checkoutRecordEntries.size() - 1);
+		int copyNum = currentEntry.getCopyNum().getCopyNum();
+		LocalDate dueDate = currentEntry.getDueDate();
+		LocalDate checkoutDate = currentEntry.getCheckoutDate();
+
+		ObservableList<CheckoutRecordEntry> data = FXCollections.observableArrayList(checkoutRecordEntries);
+		for (int i = (data.size() - 1); i > 0; i--) {
+			dueDateCol.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+			issueDateCol.setCellValueFactory(new PropertyValueFactory<>("checkoutDate"));
+			table.setItems(data);
+
+		}
+	}
+
+	public void handleSearchByMemberId() {
+
 	}
 
 }
