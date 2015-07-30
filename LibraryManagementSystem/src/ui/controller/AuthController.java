756acbf58ecb76891f66e3535c5440ded8d79650
Sujan <najussujan@gmail.com>
2015-07-29 22:47:17 -0500

    Various UI changes

--- LibraryManagementSystem/src/ui/controller/AuthController.java
+++ LibraryManagementSystem/src/ui/controller/AuthController.java
@@ -4,13 +4,22 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class AuthController {
+
+	@FXML
+	private ImageView adminView;
+	@FXML
+	private ImageView librarianView;
 
 	@FXML
 	protected void handleAdminAuth(MouseEvent event) throws IOException {
@@ -32,5 +41,35 @@ public class AuthController {
 		stage.setTitle("Main Menu");
 		stage.setScene(new Scene(mainMenuRoot));
 		stage.show();
+	}
+
+	@FXML
+	protected void addHoverAdmin() {
+		addHoverEffect(adminView);
+	}
+
+	@FXML
+	protected void removeHoverAdmin() {
+		removeHoverEffect(adminView);
+	}
+
+	@FXML
+	protected void addHoverLibrarian() {
+		addHoverEffect(librarianView);
+	}
+
+	@FXML
+	protected void removeHoverLibrarian() {
+		removeHoverEffect(librarianView);
+	}
+
+	private void addHoverEffect(ImageView imageView) {
+		// apply a shadow effect.
+		imageView.setEffect(new DropShadow(20, Color.web("#FF3300")));
+		imageView.setCursor(Cursor.HAND);
+	}
+
+	private void removeHoverEffect(ImageView imageView) {
+		imageView.setEffect(null);
 	}
 }
