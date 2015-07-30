d5c98092a2176915966125b31495470320b2d3bb
Sachin Kushwaha <sachinkeshav@gmail.com>
2015-07-29 19:46:04 -0500

    Login ruleset missed.

---
+++ LibraryManagementSystem/src/ruleset/LoginRuleSet.java
@@ -0,0 +1,26 @@
+package ruleset;
+
+import ui.controller.LoginController;
+
+public class LoginRuleSet implements RuleSet {
+	LoginRuleSet() {
+	}
+
+	@Override
+	public void applyRules(Object ob) throws RuleException {
+		LoginController controller = (LoginController) ob;
+		applyNonEmptyRule(controller);
+	}
+
+	private void applyNonEmptyRule(LoginController controller) throws RuleException {
+		String id = controller.getUserIdField().getText();
+		String password = controller.getPasswordField().getText();
+
+		if (id.isEmpty() && password.isEmpty())
+			throw new RuleException("Username and paswword field cannot be blank!");
+		if (id.isEmpty())
+			throw new RuleException("Username  field cannot be blank!");
+		if (password.isEmpty())
+			throw new RuleException("Paswword field cannot be blank!");
+	}
+}
